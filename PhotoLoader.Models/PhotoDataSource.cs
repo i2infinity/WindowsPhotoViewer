e0084611876a89d27e064bb192af740f521c0457
amrish_deep <amrishdeep@hotmail.com>
2015-01-20 19:20:43 -0600

    Initial checkin for Photo Loader SDK

--- PhotoLoader.Models/PhotoDataSource.cs
+++ PhotoLoader.Models/PhotoDataSource.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Windows.Devices.Geolocation;
-
+﻿
 namespace PhotoLoader.Models
 {
     public enum PhotoDataSource
