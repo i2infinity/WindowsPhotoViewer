71584fed93fa2e1f43ccc7b87da067f7e3c028d7
amrish_deep <amrishdeep@hotmail.com>
2015-01-21 19:52:45 -0600

    Stubbed out Unit test case for SDK

---
+++ PhotoLoaderSDK.Tests/LocalStorageLoaderTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Windows.Storage;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace PhotoLoaderSDK.Tests
+{
+    [TestClass]
+    public class LocalStorageLoaderTests
+    {
+        /// <summary>
+        /// Scenario         : [Happy path] Try retrieving photos without using any filter conditions
+        /// Expected Results : The photos selected by the user are returned
+        /// </summary>
+        [TestMethod]
+        public void RetrievePhotosHappyPath()
+        {
+            //Arrange
+
+            //Act
+
+            //Assert
+        }
+    }
+}
